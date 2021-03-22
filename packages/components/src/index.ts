5c9618cf7b098918cdb336a45dad7c2b6321c287
Fernando Rojo <frojo@sas.upenn.edu>
2021-03-22 13:12:18 -0400

    chore: export MotiView named component

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -12,3 +12,11 @@ export const Text = motify(RText)()
 export const Image = motify(RImage)()
 export const ScrollView = motify(RScrollView)()
 export const SafeAreaView = motify(RSafeAreaView)()
+
+export {
+  View as MotiView,
+  Text as MotiText,
+  Image as MotiImage,
+  ScrollView as MotiScrollView,
+  SafeAreaView as MotiSafeAreaView,
+}
