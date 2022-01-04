7e961ffd1cce448ebd122637a00682be6e4534a7
Fernando Rojo <frojo@sas.upenn.edu>
2022-01-04 12:46:54 -0500

    root import

--- examples/with-expo/index.js
+++ examples/with-expo/index.js
@@ -4,7 +4,7 @@ import 'expo/build/Expo.fx'
 import { activateKeepAwake } from 'expo-keep-awake'
 import registerRootComponent from 'expo/build/launch/registerRootComponent'
 
-import App from './_App'
+import App from './App'
 
 if (__DEV__) {
   activateKeepAwake()
