b57a69e12b1d1ac44239cb1b38a9ea7eda0efd99
Fernando Rojo <frojo@sas.upenn.edu>
2022-01-08 14:14:24 -0500

    fix: local dev alias for iOS using src

--- examples/with-expo/babel.config.js
+++ examples/with-expo/babel.config.js
@@ -1,7 +1,26 @@
-module.exports = function(api) {
-  api.cache(true);
+const aliasPackages = (...packageNames) => {
+  const alias = {}
+  for (const packageName of packageNames) {
+    alias[`moti/${packageName}`] = `../../packages/${packageName}/src`
+  }
+  return alias
+}
+
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin']
-  };
-};
+    plugins: [
+      'react-native-reanimated/plugin',
+      [
+        'module-resolver',
+        {
+          root: ['../../packages'],
+          alias: {
+            ...aliasPackages('interactions', 'skeleton', 'reorder'),
+          },
+        },
+      ],
+    ],
+  }
+}
