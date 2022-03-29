b946a787275392f0a1b8e4e019d0248aac7dd27f
Fernando Rojo <frojo@sas.upenn.edu>
2022-03-29 10:33:38 -0400

    warnings

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,5 @@
+import { Platform } from 'react-native'
+
 export { default as motify } from './motify'
 export { AnimatePresence } from 'framer-motion'
 
@@ -7,3 +9,16 @@ export { default as useAnimationState } from './use-animator'
 export { default as useDynamicAnimation } from './use-dynamic-animation'
 export * from './use-map-animate-to-style'
 export * from './constants'
+
+console.error(`@motify/core is deprecated. It has moved to 'moti'. Please import from 'moti' instead. In the next version, this will throw an error.
+
+${
+  Platform.OS == 'web'
+    ? `If you're using Next.js, you should remove all @motify/ libraries from your next-transpile-modules list inside of next.config.js. If you aren't using Next.js, you should still remove @motify/ libraries from your Webpack config's tranpile list.`
+    : ''
+}
+
+Finally, find and replace all your imports.
+
+For more info: https://github.com/nandorojo/moti/pull/136
+`)
