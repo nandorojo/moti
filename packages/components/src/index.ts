afa1114ac6ea3f8949199cb109b8606b8d16ecc0
Fernando Rojo <frojo@sas.upenn.edu>
2022-03-29 10:41:20 -0400

    fix: remove warnings

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -5,7 +5,6 @@ import {
   Image as RImage,
   ScrollView as RScrollView,
   SafeAreaView as RSafeAreaView,
-  Platform,
 } from 'react-native'
 
 const View = motify(RView)()
@@ -28,16 +27,3 @@ export {
   ScrollView,
   SafeAreaView,
 }
-
-console.error(`@motify/core is deprecated. It has moved to 'moti'. Please import from 'moti' instead. In the next version, this will throw an error.
-
-${
-  Platform.OS == 'web'
-    ? `If you're using Next.js, you should remove all @motify/ libraries from your next-transpile-modules list inside of next.config.js. If you aren't using Next.js, you should still remove @motify/ libraries from your Webpack config's tranpile list.`
-    : ''
-}
-
-Finally, find and replace all your imports.
-
-For more info: https://github.com/nandorojo/moti/pull/136
-`)
