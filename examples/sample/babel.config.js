7ab0e6f5cb44c90b04bc28a03f02b89f12e839e9
Fernando Rojo <frojo@sas.upenn.edu>
2024-02-18 14:22:28 -0500

    new example app

--- examples/sample/babel.config.js
+++ examples/sample/babel.config.js
@@ -1,6 +1,29 @@
-module.exports = function(api) {
-  api.cache(true);
+const path = require('path')
+
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+    plugins: [
+      'react-native-reanimated/plugin',
+      [
+        'module-resolver',
+        {
+          extensions: ['.tsx', '.ts', '.js', '.json'],
+          alias: {
+            // For development, we want to alias the library to the source
+            moti$: path.join(__dirname, '../../packages/moti/src/index.tsx'),
+            'moti/skeleton': path.join(
+              __dirname,
+              '../../packages/moti/src/skeleton/index.ts'
+            ),
+            'moti/interactions': path.join(
+              __dirname,
+              '../../packages/moti/src/interactions/index.ts'
+            ),
+          },
+        },
+      ],
+    ],
+  }
+}
